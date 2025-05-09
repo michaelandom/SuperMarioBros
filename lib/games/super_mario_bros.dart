66b76f06343319ce14b075043b5880c8657306c8
michaelandom <michaelandom03@gmail.com>
2025-05-10 00:30:03 +0300

    Add super_mario_bros.dart

---
+++ lib/games/super_mario_bros.dart
@@ -0,0 +1,34 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:super_mario_bros/level/level_component.dart';
+import 'package:super_mario_bros/level/level_option.dart';
+
+class SuperMarioBrosGame extends FlameGame
+    with HasCollisionDetection, HasKeyboardHandlerComponents {
+  late CameraComponent cameraComponent;
+
+  LevelComponent? _currentLevel;
+
+  @override
+  Future<void> onLoad() async {
+    cameraComponent = CameraComponent(world: world)
+      ..viewport.size = Vector2(450, 50)
+      ..viewport.position = Vector2(500, 0)
+      ..viewfinder.visibleGameSize = Vector2(450, 50)
+      ..viewfinder.position = Vector2(0, 0)
+      ..viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cameraComponent]);
+
+    loadLevel(LevelOption.lv_1_1);
+
+    return super.onLoad();
+  }
+
+  void loadLevel(LevelOption level) {
+    _currentLevel?.removeFromParent();
+    _currentLevel = LevelComponent(level);
+    add(_currentLevel!);
+  }
+}
